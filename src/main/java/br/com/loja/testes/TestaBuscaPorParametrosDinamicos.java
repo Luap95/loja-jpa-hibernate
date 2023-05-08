1bb5b08872457402b1921b6c1752a4a907f25c6f
Lucas <lucas.7795.la@gmail.com>
2023-05-08 20:14:24 -0300

    mapeamento de herança na JPA

--- src/main/java/br/com/loja/testes/TestaBuscaPorParametrosDinamicos.java
+++ src/main/java/br/com/loja/testes/TestaBuscaPorParametrosDinamicos.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class TestaBuscaPorParametrosDinamicos {
     public static void main(String[] args) {
+
         EntityManager entityManager = JPAUtil.getEntityManager();
         ProdutoDao produtoDao = new ProdutoDao(entityManager);
 
